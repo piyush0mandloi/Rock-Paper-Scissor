7ea7e8cc91bd9de0f424d34caf337c34038345c3
Piyush Mandloi <piyushmandloi474@gmail.com>
2026-06-01 12:38:50 +0530

    Version 1.2 (scores, reset button)

--- a.py
+++ a.py
@@ -1,7 +1,8 @@
 import random
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, url_for, redirect
 
 app = Flask(__name__)
+app.secret_key = "your_secret_key"
 
 choices = ["rock", "paper", "scissors"]
 
@@ -20,20 +21,33 @@ def home():
     banner_class = ""
 
     if request.method == "POST":
+
+        user_score = session.get("user_score", 0)
+        comp_score = session.get("comp_score", 0)
+        draw_score = session.get("draw_score", 0)
+
         user_input = request.form["guess"]
         comp_choice = random.choice(choices)
 
         if user_input == comp_choice:
             message = "It's a Draw!"
             banner_class = "draw"
+            draw_score+=1
 
         elif rules[user_input] == comp_choice:
             message = "You Win!"
             banner_class = "win"
+            user_score+=1
 
         else:
             message = "Computer Wins!"
             banner_class = "lose"
+            comp_score+=1
+
+        
+        session["user_score"] = user_score
+        session["comp_score"] = comp_score
+        session["draw_score"] = draw_score
 
 
     return render_template(
@@ -41,8 +55,16 @@ def home():
         message=message,
         user_choice=user_input,
         comp_choice=comp_choice,
-            banner_class=banner_class
+        banner_class=banner_class,
+        user_score = session.get("user_score", 0),
+        comp_score = session.get("comp_score", 0),
+        draw_score = session.get("draw_score", 0),
     )
+
+@app.route("/reset", methods=["GET"])
+def reset():
+    session.clear()
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
